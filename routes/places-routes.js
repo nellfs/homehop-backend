a7307ba793f0d5532501d12c9cb82d4ba4119c3d
nellfs <pokycelit@gmail.com>
2022-02-10 19:33:33 -0300

    places

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,26 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET Requests in Places");
-  res.json({ message: "It works!" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Casinha",
+    description: "Casinha bonitinha",
+    location: {
+      lat: 40.323231,
+      lgn: 20.313234,
+    },
+    address: "Rua 30 de fevereiro",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id == placeId;
+  });
+  res.json({ place });
 });
 
 module.exports = router;
